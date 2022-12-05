e517f51b31be0c58efe439b3849f6b0a1278dab3
Linas Vepstas <linasvepstas@gmail.com>
2022-12-04 18:45:32 -0600

    Expand the demo

--- examples/unifier-basic.scm
+++ examples/unifier-basic.scm
@@ -14,4 +14,31 @@
 		(Inheritance (Concept "A") (Variable "$Y"))
 		(List (Variable "$X") (Variable "$Y"))))
 
+; Run it.
 (cog-execute! joiner)
+
+; Here it is, with a more complex rewrite:
+(define join-and-make
+	(Unifier
+		(Inheritance (Variable "$X") (Concept "B"))
+		(Inheritance (Concept "A") (Variable "$Y"))
+		(Evaluation
+			(Predicate "put it together")
+			(List (Variable "$X") (Variable "$Y")))))
+
+; Run it.
+(cog-execute! join-and-make)
+
+; Because the above is so simple, it provides the same results as
+; the IdenticalLink:
+
+(define ident
+	(Bind
+		(Identical
+			(Inheritance (Variable "$X") (Concept "B"))
+			(Inheritance (Concept "A") (Variable "$Y")))
+		(Evaluation
+			(Predicate "put it together")
+			(List (Variable "$X") (Variable "$Y")))))
+
+(cog-execute! ident)
